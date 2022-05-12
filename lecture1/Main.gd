071c48a22c5404794dd70969affa97d9f83d29a1
fcole90 <fcole90@gmail.com>
2022-05-12 20:53:08 +0200

    Initial flappy bird

---
+++ lecture1/Main.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+onready var background: Sprite = $Background
+onready var ground: Sprite = $Ground
+
+# Called when the node enters the scene tree for the first tisme.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
